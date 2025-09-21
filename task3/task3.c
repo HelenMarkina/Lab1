9ae90952cd7caa6e5686d4b6d4e321dbbcd86c3f
HelenMarkina <elenamar05@mail.ru>
2025-09-21 23:20:19 +0300

    Add files via upload

---
+++ task3/task3.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	printf(" --  /|   --   --   --   --\n");
+	printf("|  |  |  |  | |  | |  | |\n");
+	printf("|  |  |  |  |  --  |  |  --\n");
+	printf("|  |  |  |  | |  | |  |    |\n");
+	printf(" --   |   --   --   --   --\n");
+}
+
